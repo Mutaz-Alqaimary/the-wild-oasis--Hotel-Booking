7013f4e91a03ceaf691193ca2d662e8cbfae8892
Mutaz-Alqaimary <mutazalqaimary5@gmail.com>
2026-01-01 22:56:00 +0300

    Edit style for button in TextExpander component to improve UX.

--- app/_components/TextExpander.tsx
+++ app/_components/TextExpander.tsx
@@ -17,7 +17,7 @@ function TextExpander({ children }: TextExpanderProps) {
     <span>
       {displayText}{" "}
       <button
-        className="text-primary-700 border-b border-primary-700 leading-3 pb-1"
+        className="text-primary-700 border-b border-primary-700 leading-3 pb-1 cursor-pointer hover:text-accent-100"
         onClick={() => setIsExpanded(!isExpanded)}
       >
         {isExpanded ? "Show less" : "Show more"}
