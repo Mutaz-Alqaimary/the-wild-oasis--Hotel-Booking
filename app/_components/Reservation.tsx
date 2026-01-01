67a896a374a43e2d562f6283eb012849949f0b32
Mutaz-Alqaimary <mutazalqaimary5@gmail.com>
2026-01-01 23:02:32 +0300

    Edit style in Reservation component

--- app/_components/Reservation.tsx
+++ app/_components/Reservation.tsx
@@ -18,7 +18,7 @@ async function Reservation({ cabin }: ReservationProps) {
   const session = await auth();
 
   return (
-    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+    <div className="grid grid-cols-2 border border-primary-800 min-h-100">
       <DateSelector
         settings={settings}
         bookedDates={bookedDates}
